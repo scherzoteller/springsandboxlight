3ee6b0d31330157612498d15f37e399a11040d3b
scherzoteller <loret.vincent@gmail.com>
2018-10-03 23:51:22 +0200

    pl

--- pic/build.pl
+++ pic/build.pl
@@ -9,7 +9,7 @@ node {
     stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
       //git 'https://github.com/scherzoteller/springsandboxlight.git'
-      scm checkout
+      checkout scm
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.           
